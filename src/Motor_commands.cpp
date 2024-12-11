f77825fc9e88ac3fac5b363e2863af26b025caf7
qz <newbiegonewild@gmail.com>
2024-12-11 20:30:02 +0800

    Added stop data in tMovement, added functionality in motor commands, revised variable names to be shorter

--- src/Motor_commands.cpp
+++ src/Motor_commands.cpp
@@ -5,10 +5,10 @@
 #include "Delay_nb.h"
 
 #define JOYSTICK_UPDATE_DELAY  500
-#define LOWER_MEDIAN_VALUE     350 // lower limit for constraining movement
-#define HIGHER_MEDIAN_VALUE    700 // higher limit for constraining movement
-#define FIRST_DIRECTION_VALUE  800 // for going forward or left
-#define SECOND_DIRECTION_VALUE 200 // for going backward or right
+#define LOWER_MEDIAN           350 // lower limit for constraining movement
+#define HIGHER_MEDIAN          700 // higher limit for constraining movement
+#define FIRST_DIRECTION        800 // for going forward or left
+#define SECOND_DIRECTION       200 // for going backward or right
 
 /**
  * @brief            Sends a message to slave depending on joystick movement
@@ -28,21 +28,25 @@ void handle_joystick( int joystick_x, int joystick_y )
 
     tMovement command;
 
-    if ( ( x_value > LOWER_MEDIAN_VALUE ) && ( x_value < HIGHER_MEDIAN_VALUE ) && ( y_value > FIRST_DIRECTION_VALUE ) )      // forward
+    if ( ( x_value > LOWER_MEDIAN ) && ( x_value < HIGHER_MEDIAN ) && ( y_value > FIRST_DIRECTION ) )                                  // forward
     {
         command = MOVEMENT_FORWARD;
     }
-    else if ( ( x_value > LOWER_MEDIAN_VALUE ) && ( x_value < HIGHER_MEDIAN_VALUE ) && ( y_value < SECOND_DIRECTION_VALUE ) ) // backward
+    else if ( ( x_value > LOWER_MEDIAN ) && ( x_value < HIGHER_MEDIAN ) && ( y_value < SECOND_DIRECTION ) )                            // backward
     {
         command = MOVEMENT_BACKWARD;
     }
-    else if ( ( x_value < SECOND_DIRECTION_VALUE ) && ( y_value > LOWER_MEDIAN_VALUE ) && ( y_value < HIGHER_MEDIAN_VALUE ) ) // right
+    else if ( ( x_value < SECOND_DIRECTION ) && ( y_value > LOWER_MEDIAN ) && ( y_value < HIGHER_MEDIAN ) )                            // right
     {
         command = MOVEMENT_RIGHT;
     }
-    else if ( ( x_value > FIRST_DIRECTION_VALUE ) && ( y_value > LOWER_MEDIAN_VALUE ) && ( y_value < HIGHER_MEDIAN_VALUE ) ) // left
+    else if ( ( x_value > FIRST_DIRECTION ) && ( y_value > LOWER_MEDIAN ) && ( y_value < HIGHER_MEDIAN ) )                             // left
     {
         command = MOVEMENT_LEFT;
+    }
+    else if ( ( x_value > LOWER_MEDIAN ) && ( x_value < HIGHER_MEDIAN ) && ( y_value > LOWER_MEDIAN ) && ( y_value < HIGHER_MEDIAN ) ) // stop
+    {
+
     }
     else
     {
